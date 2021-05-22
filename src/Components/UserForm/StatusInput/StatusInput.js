3007a2b7fd0fda8899b4ee6c25ea803a2f4af835
IronDimk <irondimk@gmail.com>
2021-05-23 01:15:30 +0700

    create header

---
+++ src/Components/UserForm/StatusInput/StatusInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './StatusInput.module.scss';
+
+const StatusInput = () => {
+    return (
+        <div className={classes.statusInput}>
+            Прежде чем действовать, надо понять
+        </div>
+    );
+};
+
+export default StatusInput;
