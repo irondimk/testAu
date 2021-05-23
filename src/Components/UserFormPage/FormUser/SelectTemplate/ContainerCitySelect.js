350a3dddbc3bfb4ae9ef31e92cca58d10d5a8307
IronDimk <irondimk@gmail.com>
2021-05-23 16:35:48 +0700

    add load cities list to select list

--- src/Components/UserFormPage/FormUser/SelectTemplate/ContainerCitySelect.js
+++ src/Components/UserFormPage/FormUser/SelectTemplate/ContainerCitySelect.js
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import citiesFromJson from '../../../../Utils/Cities/cities.json';
 import SelectTemplate from './SelectTemplate';
 
 const ContainerCitySelect = (props) => {
 
-    let bigCities; 
-    let maxPopulationCity;
+    let [bigCities, setBigCities] = useState(null);
+
+    
+    let maxPopulationCity = {
+        population: 0
+    };
 
     let filterBigCities = (cities) => {
         return cities.filter((elem)=>{
@@ -14,23 +18,43 @@ const ContainerCitySelect = (props) => {
         })
     }
 
-    useEffect(()=>{
-        bigCities = filterBigCities(citiesFromJson);
+    let sortCities = (cities) => {
 
+        let resultCities;
+
+        //find max population city
         maxPopulationCity = {
-            population: 0
+                population: 0
         };
-
-        for(let elem of bigCities){
+        debugger;
+        for(let elem of cities){
             if(Number(elem.population) > Number(maxPopulationCity.population)){
                 maxPopulationCity = {...elem}
-                console.log(2);
             }
-            
         }
-        console.log(maxPopulationCity);
 
-        console.log(bigCities);
+        //remove max population citi from arr cities
+        resultCities = cities.filter((elem)=>{
+            return elem.city != maxPopulationCity.city;
+        })
+        //sorting citites to A-Z
+        resultCities.sort((a, b)=>{
+            return a.city - b.city;
+        })
+        //add max population city to begin arr
+        resultCities.unshift(maxPopulationCity);
+
+        return resultCities;
+    }
+
+    useEffect(()=>{
+        let cities = {}; 
+        cities = filterBigCities(citiesFromJson);
+        cities = sortCities(cities);
+
+        setBigCities([...cities]);
+        // setBigCities(sortCities(bigCities));
+        
     }, [])
 
     
--- src/Components/UserFormPage/FormUser/SelectTemplate/SelectTemplate.js
+++ src/Components/UserFormPage/FormUser/SelectTemplate/SelectTemplate.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Field } from 'react-final-form';
 
 import classes from '../FormUser.module.scss';
 
 const SelectTemplate = (props) => {
+
+    let [optionsArr, setOptionsArr] = useState(""); //array options with cities list
+
+    useEffect(()=>{
+      let valueOptions;
+      if(props.data){
+        valueOptions = props.data.map((elem)=>{
+          return(
+          <option value={elem.city}>{elem.city}</option>
+          )
+        })
+        setOptionsArr([...valueOptions]);
+      }
+    }, props.data)
+    
     return (
+      
         <Field name={props.name}>
+        
             {({ input, meta }) => (
+              
               <div className={classes.form__input_block}>
                 <label className={classes.form__label}>{props.label}</label>
                 <Field className={classes.form__input} name="choosecity" component="select">
               <option />
-              <option value="#ff0000">Red</option>
-              <option value="#00ff00">Green</option>
-              <option value="#0000ff">Blue</option>
+              {optionsArr}
             </Field>
                 {meta.error && meta.touched && <span>{meta.error}</span>}
               </div>
